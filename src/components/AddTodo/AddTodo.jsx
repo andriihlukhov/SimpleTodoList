981e91b847cd89dcab5194067d33b8fabd53e480
andriihlukhov <andreyglykhov411@gmail.com>
2023-03-19 18:32:48 +0200

    first pull request

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -6,10 +6,13 @@ import { SiAddthis } from 'react-icons/si'
 
 const AddTodo = (props) => {
 
+	const [value, setValue] = useState('')
+
+
 	return (
 		<div className={s.addTodo}>
-			<MyInput children={'Write task title'} onChange={(e) => props.setValue(e.target.value)} onKeyPress={(e) => e.key == 'Enter' ? props.onAdd(props.todos.id) : ''} value={props.value}/>
-			<MyButton children={<SiAddthis size={30}/>} onClick={props.onAdd}/>
+			<MyInput children={'Write task title'} onChange={(e) => setValue(e.target.value)} value={value} onKeyPress={(e) => e.key === 'Enter' ? props.onAdd(value) & setValue('') : ''}/>
+			<MyButton className='ml-4' children={<SiAddthis size={30}/>} onClick={() => {props.onAdd(value), setValue('')}}/>
 		</div>
 	)
 }
